ece37fa19648ceaa3defd9fe3e16e0354152969f
tyago343 <tyagocasanova@hotmail.com>
2018-11-15 18:07:00 -0300

    ruta "/api/usuarios" devuelve array

---
+++ front/src/components/Search.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import TextField from '@material-ui/core/TextField';
+
+
+export default ({handleChange, inputValue}) => (
+    <form>
+        <TextField 
+            onChange={handleChange}
+            style={{width:'300px'}}
+            id="outlined-search"
+            label="Buscá por nombre y/o apellido"
+            type="search"
+            margin="normal"
+            variant="outlined"
+            //value={inputValue}
+        />
+    
+    </form>
+)
